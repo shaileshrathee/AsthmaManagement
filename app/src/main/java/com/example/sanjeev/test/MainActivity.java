07ca1cab1cca9cf2fda6b689dff71564fae5c9ff
Apoorv Malik <apoorvmalik@Apoorvs-MacBook-Pro.local>
2018-05-08 02:32:20 +0530

    changes

--- app/src/main/java/com/example/sanjeev/test/MainActivity.java
+++ app/src/main/java/com/example/sanjeev/test/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -16,40 +17,46 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
 
     Button btnSet;
-    TimePicker time;
-    DatePicker date;
-    long timeEnd;
+    long finalTime;
+    EditText day;
+    EditText hour;
+    EditText minute;
+    int day_input, hour_input, minute_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         btnSet = findViewById(R.id.btnSetAlarm);
-        date = findViewById(R.id.date_pick);
-        time = findViewById(R.id.time_picker);
+        day = findViewById(R.id.day);
+        hour = findViewById(R.id.hour);
+        minute = findViewById(R.id.minute);
 
-        date.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
-            @Override
-            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-
-            }
-        });
     }
 
     public void setAlarm(View view) {
+
+
+        day_input = Integer.parseInt(String.valueOf(day.getText()));
+        hour_input = Integer.parseInt(String.valueOf(hour.getText()));
+        minute_input = Integer.parseInt(String.valueOf(minute.getText()));
+
+        finalTime = calculateTime(day_input, hour_input, minute_input);
+
         Intent intent  = new Intent(this, AlarmReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 23432443, intent, 0);
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 1000, pendingIntent);
-        Toast.makeText(this, "Alarm will sound in 10 seconds", Toast.LENGTH_SHORT).show();
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + finalTime, pendingIntent);
+        Toast.makeText(this, "Alarm will sound in "+ String.valueOf(finalTime/1000) +" seconds", Toast.LENGTH_SHORT).show();
 
     }
 
-
-    public void getTime(View view) {
-
-        Toast.makeText(this, String.valueOf(timeEnd), Toast.LENGTH_SHORT).show();
+    private long calculateTime(int day_input, int hour_input, int minute_input) {
+        long ans = 0;
+        ans = minute_input*60*1000;
+        ans += hour_input*60*60*1000;
+        ans += day_input*24*60*60*1000;
+        return ans;
     }
 
     public void playVideo(View view) {
--- app/src/main/java/com/example/sanjeev/test/VideoPlayer.java
+++ app/src/main/java/com/example/sanjeev/test/VideoPlayer.java
@@ -11,7 +11,6 @@ public class VideoPlayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
-
         VideoView view = findViewById(R.id.video_view);
         view.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.videoplayback));
         view.start();
