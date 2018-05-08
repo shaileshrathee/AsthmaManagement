a683cc44057665ecb742aadf83656ec99d3e8a79
Sanjeev <thedevelopersanjeev@gmail.com>
2018-05-08 22:31:05 +0530

    changes

--- app/src/main/java/com/example/sanjeev/test/AlarmReceiver.java
+++ app/src/main/java/com/example/sanjeev/test/AlarmReceiver.java
@@ -1,19 +1,28 @@
 package com.example.sanjeev.test;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver{
+
     @Override
     public void onReceive(Context context, Intent intent) {
+
         Toast.makeText(context, "Alarm Sounded", Toast.LENGTH_SHORT).show();
 
-        PackageManager pm = context.getPackageManager();
-        Intent launchIntent = pm.getLaunchIntentForPackage("com.example.sanjeev.test");
-        intent.putExtra("playVideo", true);
-        context.startActivity(launchIntent);
+
+
+
+        // This will launch the specified activity of Test App
+        Intent i = new Intent();
+        i.setClassName("com.example.sanjeev.test", "com.example.sanjeev.test.VideoPlayer");
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
     }
+
+
 }
--- app/src/main/java/com/example/sanjeev/test/MainActivity.java
+++ app/src/main/java/com/example/sanjeev/test/MainActivity.java
@@ -16,52 +16,50 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnSet;
     long finalTime;
-    EditText day;
-    EditText hour;
-    EditText minute;
-    int day_input, hour_input, minute_input;
+    EditText time_input;
+    String[] hour_minute = new String[2];
+    int hour, minute;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnSet = findViewById(R.id.btnSetAlarm);
-        day = findViewById(R.id.day);
-        hour = findViewById(R.id.hour);
-        minute = findViewById(R.id.minute);
+        //Intent in = getIntent();
+        //String message = in.getStringExtra(AlarmReceiver.EXTRA_MESSAGE);
+        //Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        /*
+        if(message.equals("PlayVideo")){
+            Intent inV = new Intent(this, VideoPlayer.class);
+            startActivity(inV);
+        }
+        */
+        time_input = findViewById(R.id.time_input);
 
     }
 
     public void setAlarm(View view) {
 
+        String str = String.valueOf(time_input.getText());
+        hour_minute = str.split(":");
+        hour = Integer.parseInt(hour_minute[0]);
+        minute = Integer.parseInt(hour_minute[1]);
 
-        day_input = Integer.parseInt(String.valueOf(day.getText()));
-        hour_input = Integer.parseInt(String.valueOf(hour.getText()));
-        minute_input = Integer.parseInt(String.valueOf(minute.getText()));
-
-        finalTime = calculateTime(day_input, hour_input, minute_input);
-
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        //Toast.makeText(this, "Hour : " + String.valueOf(hour) + "Minute : " + String.valueOf(minute), Toast.LENGTH_SHORT).show();
         Intent intent  = new Intent(this, AlarmReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 23432443, intent, 0);
         AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + finalTime, pendingIntent);
-        Toast.makeText(this, "Alarm will sound in "+ String.valueOf(finalTime/1000) +" seconds", Toast.LENGTH_SHORT).show();
+        //alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 86400000, pendingIntent);
+        //alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 10, pendingIntent);
+        Toast.makeText(this, "Alarm is set successfully", Toast.LENGTH_SHORT).show();
 
     }
 
-    private long calculateTime(int day_input, int hour_input, int minute_input) {
-        long ans = 0;
-        ans = minute_input*60*1000;
-        ans += hour_input*60*60*1000;
-        ans += day_input*24*60*60*1000;
-        return ans;
-    }
 
-    public void playVideo(View view) {
-
-        Intent in = new Intent(this, VideoPlayer.class);
-        startActivity(in);
-    }
 }
