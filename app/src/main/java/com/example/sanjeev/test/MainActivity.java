79631fec58f85d3561f7831ba5a0507307b2c56a
Sanjeev <thedevelopersanjeev@gmail.com>
2018-05-12 15:59:43 +0530

    Changes

--- app/src/main/java/com/example/sanjeev/test/MainActivity.java
+++ app/src/main/java/com/example/sanjeev/test/MainActivity.java
@@ -6,9 +6,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -16,29 +13,20 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText time_input;
-    String[] hour_minute = new String[2];
+    TimePicker timePicker;
     int hour, minute;
-
+    public static String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        time_input = findViewById(R.id.time_input);
-
+        timePicker = findViewById(R.id.time_input);
     }
 
-    // This method is called, when setAlarm method is clicked
-    // It will extract time from edittext that user has entered in 24 hour format
-    // and set the alarm at that particular time which will sound daily at that time
     public void setAlarm(View view) {
-        // extracting time input as string
-        String str = String.valueOf(time_input.getText());
-        hour_minute = str.split(":");
-        // converting it to integer
-        hour = Integer.parseInt(hour_minute[0]);
-        minute = Integer.parseInt(hour_minute[1]) - 1;
+        hour = timePicker.getCurrentHour();
+        minute = timePicker.getCurrentMinute();
         // Building a calender object
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
@@ -53,5 +41,27 @@ public class MainActivity extends AppCompatActivity {
             alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 86400000, pendingIntent);
         }
         Toast.makeText(this, "Alarm is set successfully", Toast.LENGTH_LONG).show();
+    }
+
+    public void submitData(View view) {
+
+        Intent in = new Intent(this, WebPageDisplay.class);
+        url = "http://www.google.com";
+        in.putExtra("URL", url);
+        startActivity(in);
+    }
+
+    public void survey1(View view) {
+        Intent in = new Intent(this, WebPageDisplay.class);
+        url = "http://www.google.com";
+        in.putExtra("URL", url);
+        startActivity(in);
+    }
+
+    public void survey2(View view) {
+        Intent in = new Intent(this, WebPageDisplay.class);
+        url = "http://www.google.com";
+        in.putExtra("URL", url);
+        startActivity(in);
     }
 }
---
+++ app/src/main/java/com/example/sanjeev/test/WebPageDisplay.java
@@ -0,0 +1,31 @@
+package com.example.sanjeev.test;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebPageDisplay extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        webView = findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return false;
+            }
+        });
+        Intent in = getIntent();
+        String mURL = in.getStringExtra("URL");
+        webView.loadUrl(mURL);
+    }
+}
